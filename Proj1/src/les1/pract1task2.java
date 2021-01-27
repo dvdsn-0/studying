44ec95387432d161081fe36781e3e789ab9f1691
dvdsn-0 <cris-37@yandex.ru>
2021-01-27 19:35:09 +0700

    Tasks 1, 2 was edited

--- Proj1/src/les1/pract1task2.java
+++ Proj1/src/les1/pract1task2.java
@@ -6,12 +6,12 @@ import javax.swing.JOptionPane;
 public class Pract1task2 {
 
 	public static void main(String[] args) {
-        Integer a, b, c;
-        a = Integer.parseInt(JOptionPane.showInputDialog("Введите значение #1 (a): "));
-        b = Integer.parseInt(JOptionPane.showInputDialog("Введите значение #2 (b): "));
-        c = Integer.parseInt(JOptionPane.showInputDialog("Введите значение #3 (c): "));
+        Float a, b, c;
+        a = Float.parseFloat(JOptionPane.showInputDialog("Введите значение #1 (a): "));
+        b = Float.parseFloat(JOptionPane.showInputDialog("Введите значение #2 (b): "));
+        c = Float.parseFloat(JOptionPane.showInputDialog("Введите значение #3 (c): "));
         
-        JOptionPane.showMessageDialog(null, "a * 2 = " + a*2 + "\nb / 3 = " + b*3 + "\nc^2 = " + c*c + "\n(a * 2) + (b / 3) + c^2 = " + (a*2+b*3+c*c));
+        JOptionPane.showMessageDialog(null, "a * 2 = " + a*2 + "\nb / 3 = " + b/3 + "\nc^2 = " + c*c + "\n(a * 2) + (b / 3) + c^2 = " + (a*2+b/3+c*c));
 	}
 
 }
