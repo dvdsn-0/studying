74f786628ff1399bf420b92f1a638bda07e20533
dvdsn-0 <cris-37@yandex.ru>
2021-01-27 23:24:46 +0700

    Task 4 is done

--- Proj1/src/les1/Pract2task1.java
+++ Proj1/src/les1/Pract2task1.java
@@ -1,13 +1,12 @@
 package les1;
-
 import javax.swing.JOptionPane;
+
+//Дано расстояние в метрах. Найти число полных километров в нем.
 
 public class Pract2task1 {
 
 	public static void main(String[] args) {
-		JOptionPane.showMessageDialog(
-				null, "В килkометрах: " + (Float.parseFloat(JOptionPane.showInputDialog("Введите значение в метрах: "))/1000)
-				);
+	JOptionPane.showMessageDialog(null, "В киллометрах: " + (Float.parseFloat(JOptionPane.showInputDialog("Введите значение в метрах: "))/1000));
 	}
 
 }
---
+++ Proj1/src/les1/Pract2task2.java
@@ -0,0 +1,16 @@
+package les1;
+import javax.swing.JOptionPane;
+
+//Дано трехзначное число. Найти число десятков, число единиц и произведение цифр
+
+public class Pract2task2 {
+
+	public static void main(String[] args) {
+		Integer x, a, b;
+		x = Integer.parseInt(JOptionPane.showInputDialog("Введите значение: "));
+		a = x%10;
+		b = (x/10)%10;
+		JOptionPane.showMessageDialog(null, "Единицы: " + a + "\nДесятки: " + b + "\nИх произведение: " + a*b);
+	}
+
+}
