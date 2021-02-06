d8e53a1b8fba4d7d6a931517fb0e843c4ffeb6ea
dvdsn-0 <cris-37@yandex.ru>
2021-02-06 12:55:52 +0700

    task2 edited

--- Proj2/src/les5/Task2.java
+++ Proj2/src/les5/Task2.java
@@ -9,8 +9,15 @@ import java.util.Scanner;
 public class Task2 {
 
     public static void main(String[] args) {
+        int count = 0;
         String stroka = "Жили были дед и бабка";
-        System.out.println("В предложении " + (stroka.indexOf(" ") + 1) + " слов");
+
+        for (int i = 0; i < stroka.length(); i++) {
+            if (stroka.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println("В предложении " + count + " слов");
     }
 
 }
